252c07ad9ff530c2cda9bb02102bc21a0d5c03d9
Almas <aalmas2001@gmail.com>
2022-02-03 21:18:05 +0600

    Final Version

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Final Project For The React Basics Course
 
-// TODO
+During this course, we learned about a new frontend framework called __React__. This project is about an online store website which features __Fake API__ https://fakestoreapi.com/.
+***
+
+### Main Features
+* Functional components
+* React Router and Navigation
+* API requests with axios library
+* Usage of React Context
+* Redux and reducers
+* Fake database and fake authentication without real backend
+* Usage of hooks like useState, useEffect, useNavigate, useContext, useDispatch, etc.
+* Custom hooks
+* Optimization with a higher-order component - memo, useMemo and useCallback
+***
+
+### Showcase
+![](final-online-store/showcase/1.png)
+***
+![](final-online-store/showcase/2.png)
+***
+![](final-online-store/showcase/3.png)
+***
+![](final-online-store/showcase/4.png)
+***
+![](final-online-store/showcase/5.png)
--- final-online-store/src/components/profile/Profile.js
+++ final-online-store/src/components/profile/Profile.js
@@ -1,21 +1,31 @@
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { useAuthUpdate } from '../../contexts/AuthContext';
 import './Profile.css';
 
 function Profile() {
   const user = useSelector((state) => state.user);
   const setIsAuthed = useAuthUpdate();
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setIsAuthed(false);
+    navigate('/');
+  };
 
   return (
     <div className='profile'>
       <h2>Profile</h2>
       <div className="profile-data">
-        <img src={user.image} width="150" alt="" />
+        <div className="profile-data-img">
+          <img src={user.image} alt="" />
+        </div>
         <h3>Name: {user.name}</h3>
         <h3>Nickname: {user.username}</h3>
-        <h3>Wallet Balance: {user.wallet}</h3>
-        <button className="profile-button" onClick={() => setIsAuthed(false)}>Logout</button>
+        <h3>Wallet Balance: {user.wallet}$</h3>
+        <h3>Total Items in the Cart: {user.cart.length}</h3>
       </div>
+      <button className="profile-button" onClick={() => logout()}>Logout</button>
     </div>
   );
 }
